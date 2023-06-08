10db18afff88ffd3ffe4cd42cb06d606aeae0153
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2023-06-08 13:47:06 +0530

    Added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modus-angular-test-0.1.40-ng14';
+  crumbs = [
+    { id: "1", display: "Crumb 1", route: "/board" },
+    { id: "2", display: "Crumb 2", route: "/board" },
+    { id: "3", display: "Crumb 3", route: "/board" },
+    { id: "4", display: "Crumb 4", route: "/board" }
+  ];
 }
